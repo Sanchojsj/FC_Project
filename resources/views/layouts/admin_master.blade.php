55926037889cbfe3b797e37b634988ddd69d7a3e
Sanchojsj <snaranjo2020@gmail.com>
2022-08-15 19:27:50 -0500

    Commit Example

--- resources/views/layouts/admin_master.blade.php
+++ resources/views/layouts/admin_master.blade.php
@@ -16,7 +16,7 @@ Products<!DOCTYPE html>
     </head>
     <body class="sb-nav-fixed">
         <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-            <a class="navbar-brand">AgroFinca 4.0</a>
+            <a class="navbar-brand"><i class="fa-solid fa-seedling"></i> AgroFinca 4.0</a>
             <button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i class="fas fa-bars"></i></button>
             <!-- Navbar Search-->
             <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
@@ -50,7 +50,7 @@ Products<!DOCTYPE html>
                         <div class="nav">
                             <div class="sb-sidenav-menu-heading">Inicio</div>
                             <a class="nav-link" href="{{ route('dashboard') }}">
-                                <div class="sb-nav-link-icon"><i class="fa-solid fa-seedling"></i></div>
+                                <div class="sb-nav-link-icon"><i class="fa-solid fa-house"></i></div>
                                 Finca los cerezos
                             </a>
                             <div class="sb-sidenav-menu-heading">Interfaz</div>
